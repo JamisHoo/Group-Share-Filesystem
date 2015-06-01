541c641b04d2d32ec057695665a78b1352a9b58a
Jamis <hoojamis@gmail.com>
2015-06-01 23:19:53 +0800

    add access interface, serialize and deserialize interface

--- src/host.h
+++ src/host.h
@@ -41,17 +41,45 @@ public:
         }
 
         size_t id;
-        // TODO: asio address
         std::string address;
         std::string tmpdir;
         uint16_t tcp_port;
+        // TODO: is tcp port necessary?
         uint16_t ssh_port;
         // TODO: authentication information
     };
 
     size_t size() const { return _hosts.size(); }
 
+    void push(const Host& host) { _hosts.push_back(host); }
+    const Host& operator[](const size_t n) const { return _hosts[n]; }
+    Host& operator[](const size_t n) { return _hosts[n]; }
+
+    static std::string serialize(const Hosts& hosts) {
+        std::ostringstream ofs;
+        boost::archive::text_oarchive oa(ofs);
+
+        oa << hosts;
+
+        return ofs.str();
+    }
+
+    static Hosts deserialize(const std::string& byte_sequence) {
+        std::istringstream ifs(byte_sequence);
+
+        boost::archive::text_iarchive ia(ifs);
+
+        Hosts hosts;
+
+        ia >> hosts;
+
+        return hosts;
+    }
+
 private:
+    // 0 -- undefined
+    // 1 -- master's host
+    // others -- other nodes' hosts
     std::vector<Host> _hosts;
 };
 
