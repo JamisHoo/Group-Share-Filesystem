b301a75568010db2c5fff4260737b45cec453cbc
Jamis <hoojamis@gmail.com>
2015-06-04 23:19:40 +0800

    add top interface

---
+++ src/gsfs.cc
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *  Copyright (c) 2015 Jamis Hoo
+ *  Distributed under the MIT license 
+ *  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *  
+ *  Project: Group-Share Filesystem
+ *  Filename: gsfs.cc 
+ *  Version: 1.0
+ *  Author: Jamis Hoo
+ *  E-mail: hoojamis@gmail.com
+ *  Date: Jun  4, 2015
+ *  Time: 23:07:43
+ *  Description: main function
+ *****************************************************************************/
+
+#include <iostream>
+#include "../src/user_fs.h"
+#include "../src/fuse_interface.h"
+
+int main(int argc, char** argv) {
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/option_parser.h
@@ -0,0 +1,47 @@
+/******************************************************************************
+ *  Copyright (c) 2015 Jamis Hoo
+ *  Distributed under the MIT license 
+ *  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *  
+ *  Project: Group-Share Filesystem
+ *  Filename: option_parser.h 
+ *  Version: 1.0
+ *  Author: Jamis Hoo
+ *  E-mail: hoojamis@gmail.com
+ *  Date: Jun  4, 2015
+ *  Time: 23:08:38
+ *  Description: parse command-line options
+ *****************************************************************************/
+#ifndef OPTION_PARSER_H_
+#define OPTION_PARSER_H_
+
+
+#include <boost/program_options.hpp>
+
+struct OptionParser {
+
+    OptionParser() {
+        initialize();
+    }
+
+    void initialize();
+    void parse(int, char**);
+
+
+    bool is_master;
+    // IP address
+    // all nodes should be able to access each other
+    std::string address;
+    // master listens at this port
+    // other nodes connect to this port
+    uint16_t tcp_port;
+    // port for ssh, default is 22
+    uint16_t ssh_port;
+    std::string mount_point;
+    std::string tmp_dir;
+
+    boost::program_options::variables_map vm;
+    boost::program_options::options_description all_options;  
+};
+
+#endif /* OPTION_PARSER_H_ */
