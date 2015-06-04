695f19d24845c48322f673d05842d254a69f050c
Jamis <hoojamis@gmail.com>
2015-06-04 11:40:20 +0800

    add ssh manager

---
+++ src/ssh_manager.h
@@ -0,0 +1,59 @@
+/******************************************************************************
+ *  Copyright (c) 2015 Jamis Hoo
+ *  Distributed under the MIT license 
+ *  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *  
+ *  Project: Group-Share Filesystem
+ *  Filename: ssh_manager.h 
+ *  Version: 1.0
+ *  Author: Jamis Hoo
+ *  E-mail: hoojamis@gmail.com
+ *  Date: Jun  4, 2015
+ *  Time: 10:46:58
+ *  Description: 
+ *****************************************************************************/
+#ifndef SSH_MANAGER_H_
+#define SSH_MANAGER_H_
+
+#include <unordered_map>
+#include "libssh_wrapper.h"
+
+
+class SSHManager {
+    ~SSHManager() {
+        for (auto i: _connections)
+            delete i.second;
+    }
+
+    int insertHost(const uint64_t id, const std::string& addr, const uint16_t port) {
+        std::unique_ptr<SSHSession> ssh_session(new SSHSession);
+
+        if (ssh_session->initialize()) return 1;
+        if (ssh_session->setHost(addr)) return 1;
+        if (ssh_session->setPort(port)) return 1;
+        
+        SSHSession* pointer = ssh_session.release();
+
+        _connections.emplace(id, pointer);
+
+        return 0;
+    }
+
+    int removeHost(const uint64_t id) {
+        return !_connections.erase(id);
+    }
+
+    intmax_t read(const uint64_t id, const std::string& path, const size_t offset,
+                  const size_t size, char* buff) {
+        auto ite = _connections.find(id);
+        if (ite == _connections.end()) return -1;
+
+        return ite->second->read(path, offset, size, buff);
+    }
+
+private:
+    std::unordered_map<uint64_t, SSHSession*> _connections;
+};
+
+
+#endif /* SSH_MANAGER_H_ */
